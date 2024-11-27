126a4dbb19fa605b404710b2bb6e8d90ee48d3d4
D-H-O-R-A <diegoantunes2301@gmail.com>
2024-11-27 04:06:45 -0300

    update

--- ScanningApp/3D UI and Visualizations/Wireframe.swift
+++ ScanningApp/3D UI and Visualizations/Wireframe.swift
@@ -1,20 +1,14 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A visualization of the edges of a 3D box.
-*/
-
 import Foundation
 import SceneKit
 
 class Wireframe: SCNNode {
     
     private var color = UIColor.appYellow
+    private var customWireframe: SCNNode?  // Para armazenar a "segunda pele"
     
     var isHighlighted: Bool = false {
         didSet {
-            geometry?.firstMaterial?.diffuse.contents = isHighlighted ? UIColor.red : color
+            customWireframe?.geometry?.firstMaterial?.diffuse.contents = isHighlighted ? UIColor.red : color
         }
     }
     
@@ -24,13 +18,14 @@ class Wireframe: SCNNode {
     init(extent: float3, color: UIColor, scale: CGFloat = 1.0) {
         super.init()
         
-        let box = SCNBox(width: CGFloat(extent.x), height: CGFloat(extent.y), length: CGFloat(extent.z), chamferRadius: 0)
-        box.firstMaterial?.diffuse.contents = color
-        box.firstMaterial?.isDoubleSided = true
-        self.geometry = box
-        
+        // Gerar a malha que contorna o objeto, ou um "wireframe" usando pontos.
+        customWireframe = createWireframe(extent: extent)
+        if let wireframe = customWireframe {
+            wireframe.geometry?.firstMaterial?.diffuse.contents = color
+            self.addChildNode(wireframe)
+        }
+
         self.color = color
-        
         setupShader()
         
         NotificationCenter.default.addObserver(self,
@@ -38,17 +33,14 @@ class Wireframe: SCNNode {
                                                name: ObjectOrigin.movedOutsideBoxNotification,
                                                object: nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func update(extent: float3) {
-        if let box = self.geometry as? SCNBox {
-            box.width = CGFloat(extent.x)
-            box.height = CGFloat(extent.y)
-            box.length = CGFloat(extent.z)
-        }
+        // Atualiza a escala ou ajuste do contorno, se necessário
+        customWireframe?.scale = SCNVector3(extent.x, extent.y, extent.z)
     }
     
     @objc
@@ -61,12 +53,27 @@ class Wireframe: SCNNode {
         }
     }
     
-    // MARK: - Shading
+    // MARK: - Gerar o Wireframe ou Malha Contornando o Objeto
+    func createWireframe(extent: float3) -> SCNNode {
+        // Criar uma linha contornando o objeto com base em sua extensão
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: CGFloat(-extent.x / 2), y: CGFloat(-extent.y / 2)))
+        path.addLine(to: CGPoint(x: CGFloat(extent.x / 2), y: CGFloat(-extent.y / 2)))
+        path.addLine(to: CGPoint(x: CGFloat(extent.x / 2), y: CGFloat(extent.y / 2)))
+        path.addLine(to: CGPoint(x: CGFloat(-extent.x / 2), y: CGFloat(extent.y / 2)))
+        path.close()
+
+        let shape = SCNShape(path: path, extrusionDepth: CGFloat(extent.z))
+        let node = SCNNode(geometry: shape)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.appYellow
+        return node
+    }
     
+    // MARK: - Shading
     func setupShader() {
         guard let path = Bundle.main.path(forResource: "wireframe_shader", ofType: "metal", inDirectory: "art.scnassets"),
-            let shader = try? String(contentsOfFile: path, encoding: .utf8) else {
-                return
+              let shader = try? String(contentsOfFile: path, encoding: .utf8) else {
+            return
         }
         
         geometry?.firstMaterial?.shaderModifiers = [.surface: shader]
